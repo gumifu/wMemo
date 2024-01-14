5a6b73c53772375adf84e55b47795d61176c17cd
藤﨑文崇 <ffujisaki@fujitakashinoMacBook-Pro.local>
2024-01-14 17:34:28 +0900

    first commit -- build Environment

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default function App() {
+const App = (): JSX.Element => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-gray-300">
+      <Text className="text-gray-500 font-bold">Hello!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
